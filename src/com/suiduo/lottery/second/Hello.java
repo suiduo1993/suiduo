4d1e603c8975ed1a8c50937f25b9b0998f55f2a8
suiduo1993 <63390294+suiduo1993@users.noreply.github.com>
2020-04-10 11:51:28 +0800

    Update Hello.java

--- src/com/suiduo/lottery/second/Hello.java
+++ src/com/suiduo/lottery/second/Hello.java
@@ -2,6 +2,6 @@ package com.suiduo.lottery.second;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("Hello World!");
     }
 }
